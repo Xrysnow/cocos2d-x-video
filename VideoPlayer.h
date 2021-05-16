fa08f50ce02ed5624e2044e4b9856c92b3cfab68
Xrysnow <xrysnow@outlook.com>
2021-05-16 13:54:23 +0800

    add Player::getDecoder()

--- VideoPlayer.h
+++ VideoPlayer.h
@@ -10,10 +10,14 @@ namespace video
 	public:
 		enum class PlayMode
 		{
-			STEP,	   // step a const time per frame
-			REALTIME,  // realtime time -> video time, not recommand
-			FRAME,	   // game frame -> video frame
-			MANUAL,	   // invoke update manually
+			/** step a const time per frame */
+			STEP,
+			/** realtime time -> video time, not recommand */
+			REALTIME,
+			/** game frame -> video frame */
+			FRAME,
+			/** invoke update manually */
+			MANUAL,
 		};
 		static Player* create(const std::string& path);
 	protected:
@@ -23,21 +27,23 @@ namespace video
 		void vstop();
 		void vpause();
 		void vresume();
-		// can only seek key frame, not recommand to set a non-0 value
+		/** can only seek key frame, not recommand to set a non-0 value */
 		void seek(uint32_t frame);
-		// step dt in decoder and get data, will step 1 frame if dt<0
+		/** step dt in decoder and get data, will step 1 frame if dt<0 */
 		void update(float dt) override;
 
-		// see PlayMode
+		/** see PlayMode */
 		void setPlayMode(PlayMode m);
-		// used for PlayMode::STEP mode
+		/** used for PlayMode::STEP mode */
 		void setStep(double v) { step = v; }
-		// auto invoke removeFromParentAndCleanup when finished
+		/** auto invoke removeFromParentAndCleanup when finished */
 		void setAutoRemove(bool b) { autoRemove = b; }
-		// set callback when finished
+		/** set callback when finished */
 		void setVideoEndCallback(const std::function<void()>& func);
-		// save current frame to file
+		/** save current frame to file */
 		void saveCurrentFrame(const std::string& path);
+
+		Decoder* getDecoder() const { return decoder; }
 
 	protected:
 		void draw(cocos2d::Renderer* renderer, const cocos2d::Mat4& transform, uint32_t flags) override;
